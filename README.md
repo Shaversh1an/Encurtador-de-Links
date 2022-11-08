b305b118a3d84ee2508563a822e1b49835ab2dbd
Shaversh1an <jiseloo52@gmail.com>
2022-11-08 19:32:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1 align="center"> Encurtador de Links </h1>
+
+
+<p align="center">
+  <img alt="preview" src="./assets/preview2.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- GitHub
+
+## 💻 Projeto
+
+Um encurtador de links para que possamos encaminhar links complexos com mais facilidade.
---
+++ script.js
@@ -0,0 +1,43 @@
+function encurtarUrl() {
+    let url = document.getElementById("input-url").value;
+    if (!url) {
+        alert("É preciso incerir um URL para encurtar");
+        return
+    }
+
+
+    let headers = {
+        "Content-Type": "application/json",
+        "apiKey": "449398d426b84639a5e8d2d960cd09b8"
+    }
+
+
+    let linkRequest = {
+        destination: url,
+        domain: { fullName: "rebrand.ly" }
+    }
+
+    fetch("https://api.rebrandly.com/v1/links", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(linkRequest)
+    })
+
+    .then(Response => Response.json())
+    .then(json => {
+        console.log(json);
+        let inputUrl = document.getElementById("input-url");
+        inputUrl.value = json.shortUrl;
+    });
+}
+
+function copiar() {
+    let inputUrl = document.getElementById("input-url");
+
+    inputUrl.select();
+    inputUrl.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(inputUrl.value);
+
+    alert(`URL copiada: ${inputUrl.value}`)
+}
